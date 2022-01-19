f8a561d849af806b00c2f37dab569c48d2852f15
tunahan burak dirlik <87035514+tburakdirlik@users.noreply.github.com>
2022-01-19 21:06:03 +0300

    Update RestServicesApplication.java

--- Programming Notes/Java/SPRING/RestServicesApplication.java
+++ Programming Notes/Java/SPRING/RestServicesApplication.java
@@ -1,4 +1,4 @@
-//                                                            2. HAFTA CUMARTESİ
+//                                                  2. HAFTA CUMARTESİ
 
 package com.spring.restservices;
 import lombok.Builder;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.PushBuilder;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -93,18 +94,18 @@ class HelloWorldController{
                                 .build()
                 );
             }
-            //---------------------------------------------localhost:8080/members/15000---------------------------------
+            //---------------------------------------------localhost:8080/members4/15000---------------------------------
             //rest conventionlarına göre spesific bir member ı alma şekli aşağıdaki gibidir
-            @GetMapping("/members/{memberId}")
+            @GetMapping("/member4/{memberId}")
             public Member retrieveMembers4(@PathVariable Long memberId){
                 return Member.builder()
                         .memberId(memberId)
                         .name("burak")
                         .build();
             }
-            //---------------------------------------------localhost:8080/members/12------------------------------------
+            //---------------------------------------------localhost:8080/members5/12------------------------------------
 
-            @DeleteMapping("/members/{memberId}")
+            @DeleteMapping("/member5/{memberId}")
             @ResponseStatus(HttpStatus.NO_CONTENT) // Status 204: No Content, ben burda metotun boş döneceğini söylemiş oluyorum
             public void deleteMember(@PathVariable Long memberId){
                 //eğer metot void dönüyorsa @ResponseStatus --> HttpStatus.NO_CONTENT olur.
@@ -122,7 +123,7 @@ class HelloWorldController{
                         .memberId(12L)
                         .build();
             }
-            /*
+             /*
 
             POST --> localhost:8080/members    veya    localhost:8080/members?name=tunahan&id=12
 
@@ -143,56 +144,75 @@ class HelloWorldController{
                                                                                           |____________________________|
 
 
-            ____
-            |
-            |Produces response un tipi hangi tipte olacak onu gösteriyor
-            |Consumes metotun aldığı parametre hangi tipte olacak onu göösteriyor
-            |__
-                |--> bunlar @GetMappin() içersinde kullanılır. 
-                
-            Rest http nin belirli kurallarla uygulanmış halidir
-            Her rest servis http servisdir aynı zamanda, ama her http servis rest değildir.
-            Rest aslında bir mimari stildir.
+
+            produces response un tipi hangi tipte olacak onu gösteriyor
+            consumes metotun aldığı parametre hangi tipte olacak onu göösteriyor
+            rest http nin belirli kurallarla uygulanmış halidir
+            her rest servis http servisdir aynı zamanda, ama her http servis rest değildir.
+            rest aslında bir mimari stildir.
             Http nin daha derli toplu uygulanmış halidir.
-            Bittorrent yapısını mimarisi incele
+            bittorrent mimarisi incele
 
-            Spring boot application hiyerarşik olarak kendisiyle aynı seviyede olan veya alt paketlerde olan componentleri bulur ve contextlerine ekler.
+            spring boot application hiyerarşik olarak kendisiyle aynı seviyede olan veya
+            alt paketlerde olan componentleri bulur ve contextlerine ekler
 
-            @Configuration Anotasyonu Ne Yapar?
-            
-                 @Bean tanımlaması içeren fonksiyonlar içerir.
-                 Tanımlandığı fonksiyon her neyi return ediyorsa, onun context içine ekleneceğini belirtir.
-                 @Bean, genelde 3rd parti sınıflar context içine eklenmek istediğinde kullanılır.
-                 Dosya içerisinden birden çok bean tanımı olabileceğini belirtir.
-                 Eskiden config işlemleri xml dosyaları içinde yapılırken, Spring 3’ten sonra 
-                 @Configuration anotasyonuyla java sınıflarından @Bean tanımlamasıyla yapabilme özgürlüğü geldi.
-                 Runtime anında config olarak tanımlanan sınıf içindeki @Bean anotasyonlarının, spring container tarafından işlenebilmesi için belirtilir.
-                 Aslında @Configuration olarak tanımlanan sınıf içindeki @Bean tanımlamalarının her biri için app context içinde oluşturulmak üzere otomatik 
-                 xml tanımlaması yapılmış oluyor. Eğer ben tanımı XML dosyasından yapıldıysa, @ImportResource(“another-application-context.xml”) tanımlaması 
-                 ile harici bir xml import edilebilir. @Import(OtherConfiguration.class) tanımlamasıyla harici bir config import edilebilir.
+            @Configuration
 
-            Spring boot uygulamaları default olarak 8080 portunu kullanır.
-            Aynı pc de 2 tane spring boot uygulaması varsa bir tanesinin port numarasını değiştirmem gerekir.
+
+            endpoint = dışardan erişebildiğim bir nokta
+            spring boot uygulamaları default olarak 8080 portunu kullanır
+            aynı pc de 2 tane spring boot uygulaması varsa bir tanesinin port numarasını değiştirmem
+            gerekir.
 
             Bu eski kullanım --> @RequestMapping(method = RequestMethod.GET, value = "/hello")
             Bu yeni kullanım --> @GetMapping("/hello")
             ikisi de aynı
 
-            long, integer; bunlar javanın kendi dünyasına ait kavramlar.
-            Json diyo ki sayı mısın değil misin kardeşim bana onu söyle veya text misin değil misin ?
-            Json için listin veya setin de bir önemi yok. set dönmek veya list dönmek json için farketmiyor.
-            Json kullanmak spring bootun default davranışı.
-            rest tek taraflı bir iletişim
-            web socket çift taraflı bir iletişim, 2 taraf arasında bir connection açılıyor.
+            long integer bunlar javanın kendi dünyasına ait kavramlar.
+            Json diyo ki sayımısın değilmisin kardeşim bana onu söyle text misin değil misin
+            Json için listin veya setin de bir önemi yok. set dönmek veya list dönmek json için farketmiyor
+            json kullanmak spring bootun default davranışı
 
-            GELEN RESPONSE 
-            http 200 li geliyorsa başarılıdır.
-            http 300 lü bişey geliyorsa redirect olayı var
-            http 400 lü bişey geliyorsa client hatalı kullanmaya çalışmıştır o yüzden hata almıştır.
-            http 500 lü bişey geliyorsa server patlamıştır
-            
-            --> https://www.blogkafem.net/2020/05/spring-boot-bean-component-configuration-autoconfiguration-componentscan-anotasyonlari.html
- */
+              */
+            //----------------------------------------------------------------------------------------------------------
+            @PostMapping("/members/{memberId}")
+            @ResponseStatus(HttpStatus.CREATED)
+            public Member createMember4(@RequestBody Member member,
+                                        @RequestHeader("deneme-header") String header,
+                                        @PathVariable Long memberId,
+                                        @RequestParam(required = false) String memberName
+                                        ){
+                                                    return Member.builder()
+                                                            .name(member.getName())
+                                                            .memberId(12L)
+                                                            .build();
+                                         }
+
+            /*
+                                        POST --> localhost:8080/members/12?memberName=tunahan
+                                                 localhost:8080/members/12   yukardakiyle yandaki aynı şey mi bunu sor
+
+                                        Query Params
+                                            KEY		VALUE
+                                            memberName	tunahan
+
+                                        HEADERS
+                                            KEY		VALUE
+                                            deneme-header	1-2-3
+
+                                        BODY
+                                            {
+                                                    "name": "tunahan"
+                                            }
+            @RequestParam null olamaz metot içine koyduysan göndermek zorundasın ,
+            @Pathvariable kullanılmak zorunda çünkü url i şekilllendiriyor
+            @RequestParam(required=false) String memberName --> bu optional oluyor, memberName i ister gönder ister gönderme
+            @Requestbodt nin gelmesini sağlayan şey gönderdiğim body di
+            @RequestHeader ın gelmesii sağlayan şey verdiğim headerlardı
+            @Pathvariable ın gelmesini sağlayan şey url deki 12
+            @Requestparamın gelmesini sağlayan şey ? den sonraki key value çiftydi  --> memberName=tunahan
+
+            */
 //----------------------------------------------------------------------------------------------------------------------
 }
 
